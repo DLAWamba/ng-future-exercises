2250934b4960b6a109bbf2bb6c5877c3a1dadb8a
dlaw <66555238+DLAWamba@users.noreply.github.com>
2024-04-16 08:27:09 +0000

    sfsdfzx

--- src/app/exercises/components/forms/forms.component.ts
+++ src/app/exercises/components/forms/forms.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core'
-import { ReactiveFormsModule } from '@angular/forms'
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+
+// Se pueden añadir prototipos para el formulario
+interface HabitModel {
+  textBox1: string
+}
+
+//type CreateHabitForm
 
 @Component({
   selector: 'app-forms',
@@ -8,4 +15,20 @@ import { ReactiveFormsModule } from '@angular/forms'
   styleUrl: './forms.component.css',
   imports: [ReactiveFormsModule],
 })
-export class FormsComponent {}
+export class FormsComponent {
+  message: string = ''
+  habits: string[] = []
+
+  habitsForm = new FormGroup({
+    textBox1: new FormControl('', [Validators.required, Validators.minLength(2)]),
+  })
+
+  submit() {
+    if (this.habitsForm.controls.textBox1.valid) {
+      this.message = ''
+      this.habits.push(this.habitsForm.value.textBox1 ?? '')
+    } else {
+      this.message = '' + this.habitsForm.controls.textBox1.errors ?? ''
+    }
+  }
+}
